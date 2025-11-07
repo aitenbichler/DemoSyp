f193a18e45d0e5a60308725ffa4067a02f685b6b
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2025-11-07 10:56:19 +0100

    use build env

--- LeoCloud/UpdateLeocloudBackend.cmd
+++ LeoCloud/UpdateLeocloudBackend.cmd
@@ -1,12 +1,13 @@
-kubectl delete -n student-h-aitenbichler deployment demosypapi
-kubectl delete -n student-h-aitenbichler service demosypapi-svc
-kubectl delete -n student-h-aitenbichler ingress demosypapi-ingress
-kubectl delete -n student-h-aitenbichler pod -l app=demosypapi
-kubectl create -f leocloud-deploy-backend-VolumnsClaim.yaml
-kubectl create -f leocloud-deploy-backend.yaml
+set NameSpace=student-h-aitenbichler
+set AppPrefix=demosypapi
+
+kubectl delete -n %NameSpace% deployment %AppPrefix%
+kubectl delete -n %NameSpace% service %AppPrefix%-svc
+kubectl delete -n %NameSpace% ingress %AppPrefix%-ingress
+kubectl delete -n %NameSpace% pod -l app=%AppPrefix%
+kubectl create -f ./backend-leocloud-%NameSpace%-%AppPrefix%-volumnsClaim.yaml
+kubectl create -f ./backend-leocloud-%NameSpace%-%AppPrefix%.yaml
 
 pause
-:: kubectl apply -f leocloud-deploy-V2.yaml
-
 
 :: Copy from volumn => kubectl cp student-h-aitenbichler/demosypapi-79686ccb6c-8r87r:/app/Data .
